3b8b8d76fd3db72ad0dc51e9c4afbc70fb668038
Kareem Gurung <kareemgurung@Kareems-MacBook-Pro.local>
2020-11-13 13:55:14 +0000

    readmore functionality added

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Tour({id, image, info, price, name}) {
+
+    const [readMore, setReadMore] = useState(false);
     return (
-        <article className="single-tour">
-            <img src={image} alt={name}/>
-            <footer>
-                <div className="tour-info">
-                    <h4>{name}</h4>
-                    <h4 className="tour-price">£{price}</h4>
-                </div>
-                <p>{info}</p>
-                <button className="delete-btn">Not Interested</button>
-            </footer>
-        </article>
-    )
+      <article className="single-tour">
+        <img src={image} alt={name} />
+        <footer>
+          <div className="tour-info">
+            <h4>{name}</h4>
+            <h4 className="tour-price">£{price}</h4>
+          </div>
+          <p>
+            {readMore ? info : info.substring(0,200)}
+            <button onClick={() => setReadMore(!readMore)}>{readMore ? "Showless" : "Readmore"}</button>
+          </p>
+          <button className="delete-btn">Not Interested</button>
+        </footer>
+      </article>
+    );
 }
 
 export default Tour;
